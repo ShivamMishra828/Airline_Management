a63b1b08db71d8b0c2acbad8cc804faa29b786ff
ShivamMishra828 <shivam9878522087@gmail.com>
2024-05-09 13:20:32 +0530

    FEAT: Added Model and its migration file for Flights

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -1,33 +1,36 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Airplane extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Airplane extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            this.hasMany(models.Flight, {
+                foreignKey: "airplaneId",
+                onDelete: "CASCADE",
+            });
+        }
     }
-  }
-  Airplane.init(
-    {
-      modelNumber: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      capacity: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0,
-      },
-    },
-    {
-      sequelize,
-      modelName: "Airplane",
-    }
-  );
-  return Airplane;
+    Airplane.init(
+        {
+            modelNumber: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            capacity: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+            },
+        },
+        {
+            sequelize,
+            modelName: "Airplane",
+        }
+    );
+    return Airplane;
 };
