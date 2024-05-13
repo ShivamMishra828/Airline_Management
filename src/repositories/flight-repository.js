307d78a684428142e163ec0fc9251330599668a9
ShivamMishra828 <shivam9878522087@gmail.com>
2024-05-13 19:28:09 +0530

    FEAT: Added Row Lock on Flights

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require("sequelize");
 const CrudRepository = require("./crud-repository");
 const { Flight, Airplane, Airport, City } = require("../models");
+const db = require("../models");
+const { addRowLockOnFlights } = require("./queries");
 
 class FlightRepository extends CrudRepository {
     constructor() {
@@ -59,6 +61,7 @@ class FlightRepository extends CrudRepository {
     }
 
     async updateRemainingSeats(flightId, seats, dec = true) {
+        await db.sequelize.query(addRowLockOnFlights(flightId));
         const flight = await Flight.findByPk(flightId);
         if (parseInt(dec)) {
             const response = await flight.decrement("totalSeats", {
